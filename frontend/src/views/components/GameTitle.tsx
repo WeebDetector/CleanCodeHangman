7f847a2bc5dcbb7de7671910612b98a0d83d637d
Mantas Svetlauskas <svettlauskas@gmail.com>
2022-03-01 09:25:23 +0200

    [REFACT] Removing unnecessary return call

--- frontend/src/views/components/GameTitle.tsx
+++ frontend/src/views/components/GameTitle.tsx
@@ -1,22 +1,17 @@
 /* eslint-disable no-restricted-imports */
 import { Box, Typography } from "@material-ui/core";
-import React, { CSSProperties } from "react";
+import React from "react";
 
 const TITLE: string[] = ["H", "A", "N", "G", "M", "A", "N"];
 
 export const GameTitle = () => {
   return (
     <Box sx={{ display: "inline-flex" }}>
-      {TITLE.map((letter, index) => {
-        return (
-          <Typography
-            key={"title-letter-" + index}
-            variant="h1" 
-          >
-            {letter}
-          </Typography>
-        );
-      })}
+      {TITLE.map((letter, index) => (
+        <Typography key={"title-letter-" + index} variant="h1">
+          {letter}
+        </Typography>
+      ))}
     </Box>
   );
 };
