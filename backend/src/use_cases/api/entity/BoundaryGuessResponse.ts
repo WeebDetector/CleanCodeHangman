865c5f91a5f8997dab2909cab43270446a0f61b0
Mantas Svetlauskas <svettlauskas@gmail.com>
2022-03-02 11:59:20 +0200

    [FEAT] Updating BE to send over missedGuesses

--- backend/src/use_cases/api/entity/BoundaryGuessResponse.ts
+++ backend/src/use_cases/api/entity/BoundaryGuessResponse.ts
@@ -3,12 +3,14 @@ export class BoundaryGuessResponse {
     private readonly stateDescription : string
     private readonly hiddenWord : Map<number, string>
     private readonly lettersGuessed: string[]
+    private readonly missedGuesses: number
 
-    constructor(isGuessCorrect : boolean, stateDescription : string, hiddenWord : Map<number, string>, lettersGuessed: string[]) {
+    constructor(isGuessCorrect : boolean, stateDescription : string, hiddenWord : Map<number, string>, lettersGuessed: string[], missedGuesses: number) {
         this.isGuessCorrect = isGuessCorrect;
         this.stateDescription = stateDescription;
         this.hiddenWord = hiddenWord;
         this.lettersGuessed = lettersGuessed;
+        this.missedGuesses = missedGuesses;
     }
 
     getGuessState(): boolean {
@@ -24,5 +26,9 @@ export class BoundaryGuessResponse {
 
     getLettersGuessed(): string[] {
         return this.lettersGuessed;
+    }
+
+    getMissedGuesses(): number {
+        return this.missedGuesses;
     }
 }
