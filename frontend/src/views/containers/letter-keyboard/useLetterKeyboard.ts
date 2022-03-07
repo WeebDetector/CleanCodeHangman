e2f05edb2e0c27c5e2fa43f597be8702bba35342
Mantas Svetlauskas <svettlauskas@gmail.com>
2022-03-07 12:37:25 +0200

    [REFACT] Adding basic error handling using snackbar

--- frontend/src/views/containers/letter-keyboard/useLetterKeyboard.ts
+++ frontend/src/views/containers/letter-keyboard/useLetterKeyboard.ts
@@ -1,13 +1,15 @@
 /* eslint-disable no-restricted-imports */
 import { GuessController } from "../../../controllers/GuessController";
 import { ViewGuessResponse } from "../../../controllers/models/ViewGuessResponse";
+import { useObserver } from "../observer/useObserver";
 
 export default function useLetterKeyboard(
   guessController: GuessController,
   setGame: (game: ViewGuessResponse) => void
 ): (playerId: number, letterGuessed: string) => void {
+  const observer = useObserver(setGame);
   const guess = (playerId: number, letterGuessed: string) => {
-    guessController.guess(playerId, letterGuessed).subscribe(setGame);
+    guessController.guess(playerId, letterGuessed).subscribe(observer);
   };
 
   return guess;
